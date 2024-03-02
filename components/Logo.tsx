f1bf681a387a89f7f0680427de2fa1a9c504f157
wanhose <hello@wanhose.dev>
2024-03-02 13:39:24 +0100

    fix(components): logo warning due priority to load first in the page

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,6 +3,12 @@ import type { JSX } from 'react';
 
 export default function Logo(): JSX.Element {
   return (
-    <Image alt="Mango Logo" height={53} src="/images/logo.svg" width={320} />
+    <Image
+      alt="Mango Logo"
+      height={53}
+      priority
+      src="/images/logo.svg"
+      width={320}
+    />
   );
 }
