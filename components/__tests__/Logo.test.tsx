d30a2c08280f98ee9fc27f82ab40439de7cf7940
wanhose <hello@wanhose.dev>
2024-03-02 12:35:17 +0100

    feat(components): add test for logo component

---
+++ components/__tests__/Logo.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import Logo from '../Logo';
+
+describe('Logo', () => {
+  it('renders successfully', () => {
+    render(<Logo />);
+
+    const img = screen.getByAltText('Mango Logo');
+
+    expect(img).toBeInTheDocument();
+    expect(img.getAttribute('height')).toBe('53');
+    expect(img.getAttribute('loading')).toBe('lazy');
+    expect(img.getAttribute('width')).toBe('320');
+  });
+});
