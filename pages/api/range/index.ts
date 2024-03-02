5117591cdc64b0d9094dbfa21f16e3656771a532
wanhose <hello@wanhose.dev>
2024-03-02 13:31:49 +0100

    refactor(api): minor changes to fit fetch data utility response types

--- pages/api/range/index.ts
+++ pages/api/range/index.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ min: 1, max: 100 });
+  res.status(200).json({ type: 'range', min: 1, max: 100 });
 }
