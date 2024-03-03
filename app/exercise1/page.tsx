6c05fb0d9da06e08e42f4b239633bcd87dff9e93
wanhose <hello@wanhose.dev>
2024-03-03 17:47:42 +0100

    refactor(app): move queries outside range components

--- app/exercise1/page.tsx
+++ app/exercise1/page.tsx
@@ -4,17 +4,21 @@ import styles from '../page.module.css';
 import Loading from '../../components/Loading';
 import Logo from '../../components/Logo';
 import Range from '../../components/Range';
+import fetchData, { RangeData } from '../../utils/fetchData';
 
 export const metadata: Metadata = {
   title: 'Exercise 1',
 };
 
-export default function Exercise1() {
+export default async function Exercise1() {
+  const data = await fetchData('range');
+  const values = [(data as RangeData).min, (data as RangeData).max];
+
   return (
     <div className={styles.Container}>
       <Logo />
       <Suspense fallback={<Loading />}>
-        <Range type="range" />
+        <Range type={data.type} values={values} />
       </Suspense>
     </div>
   );
--- app/exercise2/page.tsx
+++ app/exercise2/page.tsx
@@ -4,17 +4,21 @@ import styles from '../page.module.css';
 import Loading from '../../components/Loading';
 import Logo from '../../components/Logo';
 import Range from '../../components/Range';
+import fetchData, { RangeFixedData } from '../../utils/fetchData';
 
 export const metadata: Metadata = {
   title: 'Exercise 2',
 };
 
-export default function Exercise2() {
+export default async function Exercise2() {
+  const data = await fetchData('fixed');
+  const values = (data as RangeFixedData).values;
+
   return (
     <div className={styles.Container}>
       <Logo />
       <Suspense fallback={<Loading />}>
-        <Range type="fixed" />
+        <Range type={data.type} values={values} />
       </Suspense>
     </div>
   );
